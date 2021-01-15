65a2555e10cf2f1182c8f710ace487b136bdfef6
Ishana Shastri <ishana@mit.edu>
2021-01-15 17:31:59 -0500

    landing page starter code

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,46 +1,46 @@
-import React, { Component } from "react";
-import GoogleLogin, { GoogleLogout } from "react-google-login";
-
 import "../../utilities.css";
 import "./Skeleton.css";
 
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+import React, { Component } from "react";
+
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "371897136705-uf976dbbp8kufvbgk2iqsak9ah4perbf.apps.googleusercontent.com";
 
-class Skeleton extends Component {
-    constructor(props) {
-        super(props);
-        // Initialize Default State
-        this.state = {};
-    }
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize Default State
+    this.state = {};
+  }
 
-    componentDidMount() {
-        // remember -- api calls go here!
-    }
+  componentDidMount() {
+    // remember -- api calls go here!
+  }
 
-    render() {
-        return ( <
-            > { " " } {
-                this.props.userId ? ( <
-                    GoogleLogout clientId = { GOOGLE_CLIENT_ID }
-                    buttonText = "Logout"
-                    onLogoutSuccess = { this.props.handleLogout }
-                    onFailure = {
-                        (err) => console.log(err) }
-                    />
-                ) : ( <
-                    GoogleLogin clientId = { GOOGLE_CLIENT_ID }
-                    buttonText = "Login"
-                    onSuccess = { this.props.handleLogin }
-                    onFailure = {
-                        (err) => console.log(err) }
-                    />
-                )
-            } { " " } <
-            p > test < /p>{" "} <
-            />
-        );
-    }
+  render() {
+    return (
+      <>
+        {" "}
+        {this.props.userId ? (
+          <GoogleLogout
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Logout"
+            onLogoutSuccess={this.props.handleLogout}
+            onFailure={(err) => console.log(err)}
+          />
+        ) : (
+          <GoogleLogin
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={this.props.handleLogin}
+            onFailure={(err) => console.log(err)}
+          />
+        )}{" "}
+        <p> test </p>{" "}
+      </>
+    );
+  }
 }
 
-export default Skeleton;
+export default Landing;
