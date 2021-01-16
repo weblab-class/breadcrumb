a1376ce67a644f2c21ee3a2e22c68a4c19cf98b9
Ishana Shastri <ishana@mit.edu>
2021-01-16 02:35:22 -0500

    added new schema features

---
+++ server/models/crumbEntry.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const crumbEntrySchema = new Schema({
+  creator_id: String,
+  title: String,
+  description: String,
+  latitude: Number,
+  longitude: Number,
+  //TODO add photo support
+});
+
+const CrumbEntry = mongoose.model("CrumbEntry", crumbEntrySchema);
+
+module.exports = CrumbEntry;
