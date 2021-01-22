72dd05007710b6b17f84a74d5908a530c1743492
Ishana Shastri <ishana@mit.edu>
2021-01-22 00:43:20 -0500

    fixed landing page formatting

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -2,8 +2,8 @@ import "../../utilities.css";
 import "./Landing.css";
 
 import GoogleLogin, { GoogleLogout } from "react-google-login";
-import React, { Component } from "react";
 import { Link, navigate } from "@reach/router";
+import React, { Component } from "react";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "371897136705-uf976dbbp8kufvbgk2iqsak9ah4perbf.apps.googleusercontent.com";
@@ -20,14 +20,18 @@ class Landing extends Component {
   }
 
   render() {
-    
     return (
       <>
         <div className="landing-container">
-        <h1>🍞</h1>
-        <h2>Breadcrumb...</h2>
-        <h3>Scatter your breadcrumbs on your next journey.</h3>
-        {/* {this.props.userId ? 
+          <img
+            className="bread-emoji"
+            src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/271/bread_1f35e.png"
+          ></img>
+          <h2>Breadcrumb...</h2>
+          <h3>
+            Scatter your breadcrumbs <br /> on your next journey.
+          </h3>
+          {/* {this.props.userId ? 
           ( <div >
           <h3>       </h3>
           <Link to={`/profile/${this.props.userId}`}> Journey to your profile </Link>
